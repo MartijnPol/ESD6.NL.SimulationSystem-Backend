cb1977c0164ec5b5f3da2f13dc1fece824e88939
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2018-06-13 14:27:20 +0200

    Added JWTToken

--- services/displacement-service.js
+++ services/displacement-service.js
@@ -1,6 +1,10 @@
 const unirest = require('unirest');
 
+
 class DisplacementService {
+
+
+
 
     /**
      * Push data to DisplacementSystem
@@ -9,19 +13,23 @@ class DisplacementService {
      * @param lon is the new longitude location
      * @param metersDriven is the amount of metersDriven
      */
-    static pushCarTrackerRule(id, lat, lon, metersDriven) {
-        unirest.post('http://192.168.25.122:77/DisplacementSystem/api/CarTrackers/AddRule')
-            .headers({'Content-Type': 'application/json'})
-            .send({
-                "id": id,
-                "lat": lat,
-                "lon": lon,
-                "date": new Date(),
-                "mdriven": metersDriven
-            })
-            .end(function () {
-                console.log('Pushed data to DisplacementSystem');
-            });
+     static pushCarTrackerRule(id, lat, lon, metersDriven) {
+        unirest.get('http://192.168.25.122:77/DisplacementSystem/api/authenticate/XD2DZafI3eq1Pg7fnYF1').end(function(res) {
+            let JwtToken = res.body["Token"];
+            unirest.post('http://192.168.25.122:77/DisplacementSystem/api/CarTrackers/AddRule')
+                .headers({'Content-Type': 'application/json'},{'Authentication': 'Bearer'+JwtToken})
+                .send({
+                    "id": id,
+                    "lat": lat,
+                    "lon": lon,
+                    "date": new Date(),
+                    "mdriven": metersDriven
+                })
+                .end(function () {
+                    console.log('Pushed data to DisplacementSystem');
+                });
+        });
+
     }
 
 }
